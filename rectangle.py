698747498fa2bc3d58698edf3c4d1c7a8760b56f
zehraturan <78978788+zehraturan@users.noreply.github.com>
2022-10-18 20:05:12 +0200

    Week5 Assignment is added

---
+++ rectangle.py
@@ -0,0 +1,19 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def perimeter(self):
+        return 2*(self.length+self.width)
+
+    def area(self):
+        return self.length*self.width
+
+    def display(self):
+        print(f'''Rectangle   Length: {self.length},
+            Width: {self.width},
+            Perimeter: {self.perimeter()}
+            Area: {self.area()}''')
+
+rec = Rectangle(12,15)
+rec.display()
