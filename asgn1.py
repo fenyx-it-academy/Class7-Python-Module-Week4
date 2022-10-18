649f216cf545ac3a307c5e872a79b5883edee9e9
gridliner <gridlineof@gmail.com>
2022-10-18 19:50:31 +0200

    last commit

--- asgn1.py
+++ asgn1.py
@@ -1,14 +1,5 @@
-# 1. Create a `School` class with instance attribute `capacity`.
-# 2. Add `students` as the class attribute. This will be a list and keep track of the students in the school.
-# 3. Create a `Student` class with attributes: `name`, `age`, `gender`
-# 4. Add `__str__` method to this class to print the objects.
-# 6. Add `add_student` method to the class. If capacity is full print error message else add the student.
 
-# 7. Add `print_students` method to print the all existing students. Loop through the students list and
-# print each student object.
-
-# 8. Create a `School` object and threee students, add first 2 students to school.
-# Print students and afterwards try to add the third student.
+# # sorry I don't get the number eight question 8. Create a `School` object and threee students, add first 2 students to school. Print students and afterwards try to add the third student.
 
 # 9. Use `__dict__` method to see attributes
 # s=[]
@@ -47,8 +38,9 @@ class student():
         self.gender=gender
     def __str__(self) :
         return f'{self.name},{self.age},{self.gender}'
-        #"name is % s, " "age is % s"  "gender is % s" % (self.name, self.age,self.gender)
+        
     
         
 student_obj=student('name','age','gender')
 print(student_obj)
+print(student_obj.__dict__)
